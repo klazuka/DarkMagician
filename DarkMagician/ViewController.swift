2ca8934b1464367777e4b9df31a3f14378632746
Keith Lazuka <kelazuka@microsoft.com>
2015-11-12 05:20:51 -0800

    Initial Commit

---
+++ DarkMagician/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DarkMagician
+//
+//  Created by Keith Lazuka on 11/12/15.
+//  Copyright © 2015 Circle38. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
